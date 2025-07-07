1525782c6fe8541de03f898204dbe5b3ef6c8688
XueFengLH <75533835+XueFengLH@users.noreply.github.com>
2025-07-07 16:42:51 +0800

    Update group_test.py

--- group_test.py
+++ group_test.py
@@ -1,22 +1,21 @@
-# 加法函数 - 由张三提交
 # 输入: a - 数字, b - 数字
 # 输出: 两数之和 (a + b)
 def add(a, b):
     return x
 
-# 减法函数 - 由李四提交
+
 # 输入: a - 数字, b - 数字
 # 输出: 两数之差 (a - b)
 def subtract(a, b):
     return x
 
-# 乘法函数 - 由王五提交
+
 # 输入: a - 数字, b - 数字
 # 输出: 两数之积 (a * b)
 def multiply(a, b):
     return x
 
-# 除法函数 - 由赵六提交
+
 # 输入: a - 数字, b - 数字
 # 输出: 两数之商 (a / b)
 # 异常: 当b=0时抛出ValueError("除数不能为零")
@@ -24,21 +23,21 @@ def divide(a, b):
 
     return x
 
-# 冒泡排序 - 由钱七提交
+
 # 输入: arr - 可排序列表
 # 输出: 升序排列后的原列表 (原地修改)
 def bubble_sort(arr):
 
     return arr
 
-# 插入排序 - 由孙八提交
+
 # 输入: arr - 可排序列表
 # 输出: 升序排列后的原列表 (原地修改)
 def insertion_sort(arr):
 
     return arr
 
-# 列表插入 - 由周九提交
+
 # 输入: lst - 列表, index - 索引位置, item - 要插入的元素
 # 输出: 插入元素后的原列表 (原地修改)
 # 注意: 索引越界时自动处理（插入到头部或尾部）
@@ -46,7 +45,7 @@ def insert_item(lst, index, item):
     
     return lst
 
-# 列表删除 - 由吴十提交
+
 # 输入: lst - 列表, item - 要删除的元素
 # 输出: 删除元素后的原列表 (原地修改)
 # 注意: 元素不存在时列表不变
@@ -54,13 +53,13 @@ def remove_item(lst, item):
 
     return lst
 
-# 求最大值 - 由郑十一提交
+
 # 输入: numbers - 数字列表
 # 输出: 列表中的最大值，空列表返回None
 def find_max(numbers):
     return 
 
-# 求最小值 - 由王十二提交
+
 # 输入: numbers - 数字列表
 # 输出: 列表中的最小值，空列表返回None
 def find_min(numbers):
